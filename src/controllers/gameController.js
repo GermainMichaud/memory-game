7b34b6c3005d01a0263b1875e064c99452fe5dae
GermainMichaud <gmichaud.dev@gmail.com>
2022-05-04 00:21:45 +0200

    feat: :sparkles: Add methods to gameController

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,8 +1,26 @@
 const path = require('path');
+const Score = require('../models/score');
 
 const gameController = {
   index: (_, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
+  },
+  highScores: async (_, res) => {
+    try {
+      const highScores = await Score.getHighScores();
+      res.json(highScores);
+    } catch (error) {
+      res.status(500).json({ error: 500, message: error.message });
+    }
+  },
+  addScore: async (req, res) => {
+    try {
+      const { name, duration } = req.body;
+      const result = await Score.addScore(name, duration);
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ error: 500, message: error.message });
+    }
   },
 };
 
