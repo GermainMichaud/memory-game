3edb5624dc0f6da9cd74e5c43ee23c7d904cb4ac
GermainMichaud <gmichaud.dev@gmail.com>
2022-05-04 00:21:59 +0200

    feat: :sparkles: Add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const gameController = require('../controllers/gameController');
 
 router.get('/', gameController.index);
+router.get('/highscores', gameController.highScores);
+router.post('/save-score', gameController.addScore);
 
 module.exports = router;
